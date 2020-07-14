ece3fee80d7115561f04c99d173e9885d37c709a
Victor Marquez <vamf2805@gmail.com>
2020-07-14 15:36:24 -0400

    Importar librerias para templates, crear carpeta templates para HTML

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
-    return 'Home'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return 'About'
 
 if __name__ == '__main__':
     app.run()
